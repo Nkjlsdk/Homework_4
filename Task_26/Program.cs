e9f0322de6d55f92b557918342c877059ff27e33
Your Name <e@w.com>
2022-08-16 17:53:55 +0300

    сумма цифр введенного числа

---
+++ Task_26/Program.cs
@@ -0,0 +1,19 @@
+﻿// ввод числа и вывод суммы цифр в числе.
+int num = Convert.ToInt32(Console.ReadLine());//Считали число
+int GetCountNumbers(int number)
+{
+    int count=0;//счетчик
+    int sum  = 0;
+    
+while (number>0)
+{    
+    sum = sum + number%10;
+    number/=10;//number/10=number;
+    count++;
+
+
+}
+return sum;
+}
+
+Console.WriteLine($"сумма цифр в числе {num} = {GetCountNumbers(num)}");
