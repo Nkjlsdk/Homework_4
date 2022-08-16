0727049e90d623ceb50435267d1d4d9540ad6a07
Your Name <e@w.com>
2022-08-16 18:04:25 +0300

    ввод массива и вывод его элементов

---
+++ Task_27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ввести массив их восьми элементов, вывести массив
+int [] GetMultiArray (int size)
+{
+int [] arraynum = new int [size];
+for (int i = 0; i < 8; i++)
+{
+    arraynum[i]=Convert.ToInt32(Console.ReadLine());
+}
+
+return arraynum;
+}
+   
+    Console.WriteLine ($"Массив: [{String.Join ("; ", GetMultiArray(8))}]");
