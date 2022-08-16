54c0040804f99e3ba72c513af5c790dbdfddcd8f
Your Name <e@w.com>
2022-08-16 17:28:08 +0300

    Ввод двух чисел, дейсвие A^B

---
+++ Task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿// ввод двух чисел (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int result = Convert.ToInt32(Math.Pow(A,B));
+
+Console.WriteLine($"{A}, {B}  {result}");
