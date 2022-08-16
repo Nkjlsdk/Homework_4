b4827e459271cc66ceca3f7cae5e1cde7d5621ba
Your Name <e@w.com>
2022-08-16 17:47:41 +0300

    создать цикл A^B

---
+++ Task_25_add/Program.cs
@@ -0,0 +1,16 @@
+﻿// цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int increase = 1;
+
+for (int i = 0; i < B; i++)
+{
+    increase *= A;
+}
+
+Console.WriteLine($"{A}, {B}  {increase}");
